d9511a89ccb6bd9ff4aa4c76895733e7c87dfee3
juliamontenegro <juliamontenegro1402@gmail.com>
2021-10-29 17:37:34 -0300

    Atividade Crud 2

--- blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -6,10 +6,15 @@ import java.util.Optional;
 import org.generation.blogPessoal.model.Postagem;
 import org.generation.blogPessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,17 +28,28 @@ public class PostagemController {
 		private PostagemRepository repository;
 		
 		@GetMapping
-		public ResponseEntity<List<Postagem>>findAllPostagens() {
+		public ResponseEntity<List<Postagem>> findAllPostagens(){
 			return ResponseEntity.ok(repository.findAll());
 		}
 		
-		@GetMapping ("/PorId/{id}") 
-		public Optional<Postagem>findById(@PathVariable("id")Long id) {
-			return repository.findById(id);
+		@GetMapping("/porTitulo/{titulo}")
+		public ResponseEntity<Postagem> getByTitulo(@PathVariable("titulo") String titulo){
+			return ResponseEntity.ok(repository.getByTituloContainingIgnoreCase(titulo));
 		}
 		
-		@GetMapping ("/PorTitulo/{titulo}") 
-		public Postagem findById(@PathVariable("titulo")String titulo) {
-			return repository.getByTitulo(titulo);
+		@PostMapping
+		public ResponseEntity<Postagem> post (@RequestBody Postagem postagem){
+			return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
 		}
+		
+		@PutMapping
+		public ResponseEntity<Postagem> put (@RequestBody Postagem postagem){
+			return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+		}
+		
+		@DeleteMapping("/{id}")
+		public void delete(@PathVariable Long id) {
+			repository.deleteById(id);
+		}
+		
 }
--- blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
+++ blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 	
-	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
 	
-	public Postagem getByTitulo(String titulo);
+	public Postagem getByTituloContainingIgnoreCase(String titulo);
 }
